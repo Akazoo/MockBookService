b796ffa152e55d0a6ee01485691562d5eeff75de
akazoo <pawel.kucharski1996@interia.pl>
2021-05-15 12:10:39 +0200

    done

--- src/main/java/pl/coderslab/Controller/BookController.java
+++ src/main/java/pl/coderslab/Controller/BookController.java
@@ -1,20 +1,50 @@
 package pl.coderslab.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import pl.coderslab.Model.Book;
+import pl.coderslab.Service.MemoryBookService;
 
+import java.util.List;
+
+@Controller
 @RestController
 @RequestMapping("/books")
 
 public class BookController {
 
+    @Autowired
+    private MemoryBookService memoryBookService;
 
-    @RequestMapping("/helloBook")
+    @GetMapping
+    public List<Book> getBooks() {
 
-    public Book helloBook() {
-        return new Book(1L, "9788324631766", "Thinking in Java",
-                "Bruce Eckel", "Helion", "programming");
+        return memoryBookService.getBooks();
+    } //done
 
-    }
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+
+        return memoryBookService.addBook(book);
+    } //done
+
+    @GetMapping("/{id:\\d+}")
+    public Book getBookById(@PathVariable Long id) {
+
+        return memoryBookService.getBookById(id);
+    } //done
+
+    @PutMapping
+    public Book editBook(@RequestBody Book book) {
+
+        return memoryBookService.editBook(book);
+    } //done
+
+    @DeleteMapping("/{id:\\d+}")
+    public Book deleteBook(@PathVariable Long id) {
+
+        return memoryBookService.deleteBook(id);
+    } //done
+
 }
---
+++ src/main/java/pl/coderslab/Service/MemoryBookService.java
@@ -0,0 +1,84 @@
+package pl.coderslab.Service;
+
+import org.springframework.stereotype.Service;
+import pl.coderslab.Model.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemoryBookService {
+
+    List<Book> bookList;
+    private static Long nextId = 4L;
+
+    public MemoryBookService() {
+        bookList = new ArrayList<>();
+        bookList.add(new Book(1L, "9788324631766", "Thinking in Java", "Bruce Eckel", "Helion", "programming"));
+        bookList.add(new Book(2L, "9788324627738", "Rusz glowa Java.", "Sierra Kathy, Bates Bert", "Helion", "programming"));
+        bookList.add(new Book(3L, "9780130819338", "Java 2. Podstawy", "Cay Horstmann, Gary Cornell", "Helion", "programming"));
+    }
+
+    public List<Book> getBooks() {
+
+        return List.copyOf(bookList);
+    } //done
+
+    public Book editBook(Book book) {
+
+        Optional<Book> book1 = find(book.getId());
+
+        if (book1.isPresent()) {
+            Book book2 = book1.get();
+            int index = bookList.indexOf(book2);
+
+            if (book.getIsbn() != null) {book2.setIsbn(book.getIsbn());}
+            if (book.getAuthor() != null) {book2.setAuthor(book.getAuthor());}
+            if (book.getPublisher() != null) {book2.setPublisher(book.getPublisher());}
+            if (book.getTitle() != null) {book2.setTitle(book.getTitle());}
+            if (book.getType() != null) {book2.setType(book.getType());}
+
+            bookList.set(index, book2);
+            return book2;
+        } else {
+            return new Book();
+        }
+    }
+
+    public Book deleteBook(Long id) {
+
+        Optional<Book> book = find(id);
+
+        if (book.isPresent()) {
+            bookList.remove(book.get());
+            return book.get();
+        } else {
+            return new Book();
+        }
+    } //done
+
+    public Book getBookById(Long id) {
+
+        Optional<Book> book = find(id);
+        return book.orElseGet(Book::new);
+
+    } //done
+
+    public Book addBook(Book book) {
+
+        book.setId(nextId);
+        bookList.add(book);
+        nextId++;
+
+        return book;
+    } //done
+
+    public Optional<Book> find(Long id) {
+
+        return bookList.stream()
+                .filter(x -> x.getId().equals(id))
+                .findFirst();
+    } //done
+
+}
